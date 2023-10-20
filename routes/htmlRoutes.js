43a4b8c739815e96e10fb366ca20502de2c69848
beth deutmeyer <bethdeutmeyer@gmail.com>
2023-10-20 07:13:10 -0500

    All working, comments added, README complete

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,12 +1,13 @@
 const htmlRte = require('express').Router();
 const path = require('path');
 
-
+// Get request for notes page, sends notes.html
 htmlRte.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
     console.info(`${req.method} request received`);
 });
 
+// Wild card get request, sends index.html
 htmlRte.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
     console.info(`${req.method} request received`);
